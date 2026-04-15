9f21df38d0995ff0a44af065e6796ce2c4330a2d
humbertdz <l21te0133@teziutlan.tecnm.mx>
2026-04-15 17:47:58 -0600

    Refactor contratoAYEActions: replace hardcoded token with variable for improved security

--- src/API/reaseguro/contratos/accidentes_enfermedades/nuevo/contratoAYE.actions.ts
+++ src/API/reaseguro/contratos/accidentes_enfermedades/nuevo/contratoAYE.actions.ts
@@ -1,10 +1,11 @@
 export const contratoAYEActions = () => {
+  const token = ''
   fetch(
     "http://54.82.165.213:9080/ws_reaseguro_contratos_aye/api/v1/CaeGeneralesContratoRest/getAllRecords",
     {
       method: "POST",
       headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJiZXRvIiwiaWF0IjoxNzc2MjczNTMxLCJleHAiOjE3NzYyNzUzMzF9.QdbBwhjlUrvXyfoyo-XQaMtYt-37oHNCADB1KGzmDgA`,
+        Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
       },
     },
